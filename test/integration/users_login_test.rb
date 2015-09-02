7de06baa5626db6aed02d77519c541b595d8afa0
Tyler Mays <richard.tyler.mays@gmail.com>
2015-09-02 11:06:36 -0700

    Test Users valid login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -10,5 +14,21 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?, 'Bad login flash message still appearing'   
+  end
+
+  test "login with valid information" do
+    # Visit the login path.
+    get login_path
+    # Post valid information to the sessions path.
+    post login_path, session: { email: @user.email, password: "password" }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    # Verify that the login link disappears.
+    assert_select "a[href=?]", login_path, count: 0
+    # Verify that a logout link appears
+    assert_select "a[href=?]", logout_path
+    # Verify that a profile link appears.
+    assert_select "a[href=?]", user_path(@user)
   end
 end
