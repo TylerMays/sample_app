dbb88710d62526517c1e41b1158b281fae19cee7
Tyler Mays <richard.tyler.mays@gmail.com>
2015-09-15 21:10:28 -0700

    Define Users destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [ :index, :edit, :update]
+  before_action :logged_in_user, only: [ :index, :edit, :update, :destroy ]
   before_action :correct_user,   only: [:edit, :update]
 
   def index
@@ -35,6 +35,13 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    flash[:success] = "#{user.name} deleted"
+    redirect_to users_url
   end
 
   private
