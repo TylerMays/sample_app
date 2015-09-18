df96c2c0955052ae375bfbc26d3c165ed10dbe92
Tyler Mays <richard.tyler.mays@gmail.com>
2015-09-18 11:55:19 -0700

    Route account_activations with edit action only

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,18 @@ Rails.application.routes.draw do
   
   root 'static_pages#home'
 
-  get 'signup' => 'users#new'
+  get    'signup'  => 'users#new'
 
-  get 'help'    => 'static_pages#help'
-  get 'about'   => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
+  get    'help'    => 'static_pages#help'
+  get    'about'   => 'static_pages#about'
+  get    'contact' => 'static_pages#contact'
   
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
   resources :users
-
-  get    'login'  => 'sessions#new'
-  post   'login'  => 'sessions#create'
-  delete 'logout' => 'sessions#destroy'
-
+  resources :account_activations, only: [:edit]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
